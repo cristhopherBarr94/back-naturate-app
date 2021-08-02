cf8c19a3eeded55c1fead75a734a628166863664
Cristhopher <u1802795@unimilitar.edu.co>
2021-08-02 10:10:14 -0500

    add Access-Control-Allow-Credentials true

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
+  res.setHeader("Access-Control-Allow-Credentials", true);
   // add the http metohds allowed
   res.setHeader(
     "Access-Control-Allow-Methods",
