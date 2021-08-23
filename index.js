f1c9d05f8df458cfe9bea19e76bc2a5c05c15d30
Cristhopher <u1802795@unimilitar.edu.co>
2021-08-23 12:31:40 -0500

    .

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ mongoose.connection.on("error", (err) => {
 
 // express configuration
 // set max size limit for request
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json());
 app.use(
   express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
 );
