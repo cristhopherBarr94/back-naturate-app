7ba01bd70ae6bbde5fa70264d16d581ce4540f52
Cristhopher <u1802795@unimilitar.edu.co>
2021-08-23 12:25:18 -0500

    .

--- index.js
+++ index.js
@@ -42,11 +42,7 @@ mongoose.connection.on("error", (err) => {
 
 // boody parser configuration
 // set max size limit for request
-app.use(
-  bodyParser.json({
-    limit: "50mb",
-  })
-);
+app.use(bodyParser.json());
 
 app.use(
   bodyParser.urlencoded({
