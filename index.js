7247a7870272d5eca528ea79c3e340b2fbe531f6
Cristhopher <u1802795@unimilitar.edu.co>
2021-08-24 15:19:24 -0500

    .

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use("/images", express.static(path.join("./images")));
 
 // boody parser configuration
 // set max size limit for request
-app.use(express.urlencoded({ limit: "50mb" })).use(express.json());
+app.use(express.urlencoded()).use(express.json());
 
 // implement cors middleware
 app.use(
